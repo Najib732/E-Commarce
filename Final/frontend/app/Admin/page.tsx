77783968e3e90956a5a8c909c710405667196488
Najib Mahfuj <22-48248-2@student.aiub.edu>
2025-09-17 09:12:51 +0600

    update

--- Final/frontend/app/Admin/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AdminHomePage() {
-  return (
-    <div className="p-6">
-      <button className="bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-700 transition">
-        Order List Upgrade
-      </button>
-    </div>
-  );
-}
